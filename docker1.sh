8c6e6a27dd39f22ee1ccf0c4159f6b5ee81c4311
Oliver1243 <oliverbenettmarketing@gmail.com>
2025-01-12 10:24:04 +0200

    Update docker1.sh

--- docker1.sh
+++ docker1.sh
@@ -3,11 +3,11 @@
 # Étape 1 : Créer et lancer un conteneur Docker Ubuntu 24.04 avec systemd
 sudo docker run -d -it --name ubuntu2404-systemd --privileged --cgroupns=host --tmpfs=/run --tmpfs=/tmp --volume=/sys/fs/cgroup:/sys/fs/cgroup:rw trfore/docker-ubuntu2404-systemd:latest
 
-# Étape 2 : Accéder au conteneur
-sudo docker exec -it ubuntu2404-systemd /bin/bash <<'EOF'
-  # Attendre 10 secondes avant d'exécuter les commandes
-  sleep 10
+# Étape 2 : Exécuter la commande docker exec
+sudo docker exec -it ubuntu2404-systemd /bin/bash &
 
-  # Mettre à jour les paquets et installer wget et git
-  apt update && apt install wget git -y
-EOF
+# Étape 3 : Attente de 10 secondes
+sleep 10
+
+# Étape 4 : Exécuter apt update et apt install dans le conteneur
+apt update && apt install wget git -y
